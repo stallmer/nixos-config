66ec8f611f1d2e069f2f25fd7b4ecfa9a04ed3d1
Stephen Daunheimer <469447+stallmer@users.noreply.github.com>
2024-10-14 08:55:49 -0700

    removed some default gnome keybindings that were affecting pop-shell.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -178,6 +178,19 @@
       tile-move-right-global = ["<Super><Shift>l"];
     };
 
+    # Remove defaults gnome application launch keybinds
+    "org/gnome/shell/keybindings" = {
+      switch-to-application-1 = [];
+      switch-to-application-2 = [];
+      switch-to-application-3 = [];
+      switch-to-application-4 = [];
+      switch-to-application-5 = [];
+      switch-to-application-6 = [];
+      switch-to-application-7 = [];
+      switch-to-application-8 = [];
+      switch-to-application-9 = [];
+    };
+
     # Keybinds for workspace movement and windows movement between workspaces
     "org/gnome/desktop/wm/keybindings" = {
       # Move between workspaces
