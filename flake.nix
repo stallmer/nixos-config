bb2a6b8839ab9a5c987554ed608b4cfc86bcf3fb
Stephen Daunheimer <469447+stallmer@users.noreply.github.com>
2024-10-14 21:38:23 -0700

    correcting home-manager url

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-unstable";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Configure keybindings
