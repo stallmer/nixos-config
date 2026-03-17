7c53291424214edcc98ff327a399929106b3b3f0
Stephen Daunheimer <469447+stallmer@users.noreply.github.com>
2026-03-16 20:34:29 -0700

    added an initial config for framework laptop to flake

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
     };
   };
 
+  # Configuration for Thinkpad X1 Gen11
   outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.carbon = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
@@ -23,6 +24,23 @@
             backupFileExtension = "backup";
           };
         }
+      ];
+    };
+
+    # Configuration for Framework 13 laptop
+    nixosConfiguration.framework = nixpkgs.libe.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+	home-manager.nixosModules.home-manager
+	{
+	  home-manager = {
+	    useGlobalPkgs = true;
+	    useUserPackages = true;
+	    users.stephen = import ./home.nix;
+	    backupFileExtension = "backup";
+	  };
+	}
       ];
     };
   };
