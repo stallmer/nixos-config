c583703e13fbf5bebc729f7446fb9aaefec69a91
Stephen Daunheimer <469447+stallmer@users.noreply.github.com>
2026-03-27 08:33:19 -0700

    enable pipewire and install pavucontrol

--- configuration.nix
+++ configuration.nix
@@ -60,9 +60,18 @@
     hyprlock
     hypridle
     hyprpaper
+    pavucontrol
   ];
 
   services.tailscale.enable = true;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
