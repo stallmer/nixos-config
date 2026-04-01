507a960856745991b8d0edc07d76bcb912e080b0
Stephen Daunheimer <469447+stallmer@users.noreply.github.com>
2026-04-01 09:50:16 -0700

    restructuring for multiple hosts

--- flake.nix
+++ flake.nix
@@ -11,20 +11,39 @@
 
   # Configuration for Thinkpad X1 Gen11
   outputs = { self, nixpkgs, home-manager, ... }: {
-    nixosConfigurations.nixnuc= nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.stephen = import ./home.nix;
-            backupFileExtension = "backup";
-          };
-        }
-      ];
+    nixosConfigurations = {
+      # Office NUC
+      nixnuc = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/nixnuc/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.stephen = import ./home.nix;
+              backupFileExtension = "backup";
+            };
+          }
+        ];
+      };
+      # 11th Gen Carbon X1
+      carbon = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/carbon/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.stephen = import ./home.nix;
+              backupFileExtension = "backup";
+            };
+          }
+        ];
+      };
     };
   };
 }
