61afe52ba30d40ff43380f438ac402e6b5677d31
Stephen Daunheimer <469447+stallmer@users.noreply.github.com>
2026-04-01 10:56:10 -0700

    changed cursors for hyprland

--- home.nix
+++ home.nix
@@ -47,4 +47,11 @@ in
     tealdeer
     chromium
   ];
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 24;
+  };
 }
