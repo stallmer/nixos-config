59890f7a225691a49b02175191182e37b5c7664b
Stephen Daunheimer <469447+stallmer@users.noreply.github.com>
2026-04-01 09:32:15 -0700

    adding configuration for NUC

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
 
   # Configuration for Thinkpad X1 Gen11
   outputs = { self, nixpkgs, home-manager, ... }: {
-    nixosConfigurations.carbon = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixnuc= nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
@@ -24,23 +24,6 @@
             backupFileExtension = "backup";
           };
         }
-      ];
-    };
-
-    # Configuration for Framework 13 laptop
-    nixosConfiguration.framework = nixpkgs.libe.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-	home-manager.nixosModules.home-manager
-	{
-	  home-manager = {
-	    useGlobalPkgs = true;
-	    useUserPackages = true;
-	    users.stephen = import ./home.nix;
-	    backupFileExtension = "backup";
-	  };
-	}
       ];
     };
   };
